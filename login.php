f584c951ccb32ebe478099e3ae538dd54c9709b3
prandp <32594507+prandp@users.noreply.github.com>
2018-05-28 20:57:32 +0530

    Portfolio website 1
    Sample portfolio website

--- login.php
+++ login.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Mr. Das's website</title>
-	<link rel="stylesheet" type="text/css" href="styles/main.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-</head>
-<body>
-	
-		<h1>Loading...
-		</h1>
-	
-
-</body>
-	
-</html>
+<img src="images/knockknockfactory404.jpg" width=100%>
