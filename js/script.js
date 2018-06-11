c5b48e38d709a06bbabe88407f00e0063dd6fb19
prandp <32594507+prandp@users.noreply.github.com>
2018-06-11 11:36:59 +0530

    bigfilling
    Assignment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,70 @@
+
+
 $(function(){
 	new WOW().init();
 });
+
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function(){
+  overlay.style.display = 'none';
+})
+/*$('.main-iso').isotope({
+	itemSelector:'.item',
+	layoutMode:'fitRows'
+});
+
+$('iso-nav ul li').click(function(){
+	$('.iso-nav ul li').removeClass('active');
+	$(this).addClass('active');
+
+	var selector = $(this).attr('data-filter');
+	$('main-iso').isotope({
+		filter: selector;
+	});
+	return flase;
+});
+$('.main-iso').isotope({
+	itemSelector:'.item',
+	layoutMode:'fitRows'
+});
+
+$('iso-nav ul li').click(function(){
+	$('.iso-nav ul li').removeClass('active');
+	$(this).addClass('active');
+
+	var selector = $(this).attr('data-filter');
+	$('main-iso').isotope({
+		filter: selector;
+	});
+	return flase;
+});
+
+
+
+
+
+   var typed = new Typed(".type", {
+  strings: [
+  "I am",
+  "a developer",
+  "a designer",
+  "and an artist"
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop:true,
+  
+});
+
+document.getElementById("knowmorebtn").onclick = function(){
+	location.href = "main.html";
+}; 
+document.getElementById("p2-viewsite").onclick = function(){
+	location.href = "images/underconstruction.jpg";
+}; */    
+//--------------------------------------------- porfolio.js -------------------------------------//
+//--------------------------------------------- isotop.js -------------------------------------//
+
+
 
